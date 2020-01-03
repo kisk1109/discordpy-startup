d58d731ea1889db3ecfc2bd3c6bc5f81c3ef0f08
kisk1109 <41935642+kisk1109@users.noreply.github.com>
2020-01-03 16:16:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,10 +25,7 @@ async def まじ反応しろ(ctx):
 async def test(ctx):
     await ctx.send('!join')
     await ctx.send('!play https://www.youtube.com/watch?v=s582L3gujnw')
-    
-@bot.command()
-async def what(ctx)
-     await ctx.send('ヤバイわよ！', { file: { attachment: tenor.gif } })
+
     
 @client.event
 async def on_ready():
