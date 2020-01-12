001c7246abc645b814ba30a351bacefcce2e19af
kisk1109 <41935642+kisk1109@users.noreply.github.com>
2020-01-12 22:25:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,34 +27,35 @@ async def daipan(ctx):
     await ctx.send('ヤバイわよ！',file=discord.File('tenor.gif'))
     
 @bot.command()
-async def gomi(ctx):
+async def p(ctx,age1,age2):
     voice_client = ctx.message.guild.voice_client
-    if not discord.opus.is_loaded(): 
-    #もし未ロードだったら
-        discord.opus.load_opus("heroku-buildpack-libopus")
-    #voicechannelを取得
     vc = ctx.author.voice.channel
+    music = "gomikasu.wav"
+    if not discord.opus.is_loaded(): 
+        discord.opus.load_opus("heroku-buildpack-libopus")
+    if message.author.voice_channel is None:
+        await client.send_message(message.channel ,'ボイスチャンネルに参加した上で、もう一度実行してください。')
+        return
+    if voice == None:
+        voice = await client.join_voice_channel(message.author.voice_channel)
+    # 接続しているかを確認
+    elif(voice.is_connected() == True):
+        # 接続先で音声を流しているか確認
+        if(player.is_playing()):
+            # 流していれば停止させる
+            player.stop()
     #voicechannelに接続
     await ctx.author.voice.channel.connect()
-    source = discord.FFmpegPCMAudio("gomikasu.wav")
+    if age1 == gomi:
+        music = "gomikasu.wav"
+    if age1 == gaku:
+        music = "gaku-otousandayo.wav"
+    source = discord.FFmpegPCMAudio(music)
     ctx.message.guild.voice_client.play(source)
-
-    
-# ping-通信速度を測る
-@bot.command()
-async def ping(ctx):
-    before = time.monotonic()
-    embed = discord.Embed(description="Pong!")
-    msg = await ctx.send(embed=embed)
-    ping = (time.monotonic() - before) * 1000
-       
-    pingmsg = "反応速度:\n{0}ms".format(int(ping))
-    embed = discord.Embed(description=pingmsg)
-    await msg.edit(embed=embed)
 
 @client.event
 async def on_ready():
-    CHANNEL_ID = 658980967876263936# 任意のチャンネルID(int)
+    CHANNEL_ID = '658980967876263936' # 任意のチャンネルID(int)
     channel = client.get_channel(CHANNEL_ID)
     await channel.send('BOT起動')
     
