4d448104c6fcc7b18f823f26c7494cb2b7643110
kisk1109 <41935642+kisk1109@users.noreply.github.com>
2020-03-16 21:57:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,10 @@
 from discord.ext import commands
+from asyncio import sleep
 import os
 import traceback
 import discord
 import re 
+
 
 bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -34,14 +36,6 @@ async def gomi(ctx):
     await ctx.author.voice.channel.connect()
     source = discord.FFmpegPCMAudio("gomikasu.wav")
     ctx.message.guild.voice_client.play(source)
-        
-@bot.command()
-async def cleanup(cmd):
-        if cmd.author.guild_permissions.administrator:
-            await cmd.channel.purge()
-            await cmd.channel.send('塵一つ残らないね！')
-        else:
-            await cmd.channel.send('何様のつもり？')
             
 @bot.command()
 async def 全部消えちゃえ(cmd):
@@ -50,6 +44,43 @@ async def 全部消えちゃえ(cmd):
             await cmd.channel.send('テキストチャンネルを灰にしてしまいました')
         else:
             await cmd.channel.send('何様のつもり？')
+            
+# BOT動作プログラム
+@client.event
+async def on_message(message):
+    # 送り主がBotだった場合反応したくないので
+    if client.user != message.author:
+        # 削除コマンド
+        if message.content.startswith("!delchat "):
+            #役職比較
+            if cmd.author.guild_permissions.administrator:
+                msgch = client.get_channel(658980967876263936)
+                # メッセージを格納
+                delcmd = message.content
+                # 入力メッセージのリスト化
+                delcmd_ = delcmd.split()
+                # 入力メッセージのint化
+                delcmd_int = int(delcmd_[1])
+                # 入力メッセージの単語数
+                delcmd_c = len(delcmd_)
+                if delcmd_c == 2 and delcmd_int <= 50 and delcmd_int > 1:
+                    # メッセージ取得
+                    msgs = [msg async for msg in client.logs_from(message.channel, limit=(delcmd_int+1))]
+                    await client.delete_messages(msgs)
+                    delmsg = await msgch.send('削除が完了しました')
+                    await sleep(5)
+                    await client.delete_message(delmsg)
+                else:
+                    # エラーメッセージを送ります
+                    delmsg = await msgch.send( "コマンドが間違っています。[!delchat *] *:2～50")
+                    await sleep(5)
+                    await client.delete_message(delmsg)
+                    
+            else:
+                # エラーメッセージを送ります
+                delmsg = await msgch.send("admin権限がありません。")
+                await sleep(5)
+
 
 
 @client.event 
